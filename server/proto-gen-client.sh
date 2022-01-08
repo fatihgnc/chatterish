56f2c8d5fb6fcb7fc739ab80f3852c71225d2db5
Fatih Genç <10bachiko@gmail.com>
2022-01-08 22:58:30 +0300

    change proto file name

--- server/proto-gen-client.sh
+++ server/proto-gen-client.sh
@@ -1,4 +1,4 @@
 mkdir -p ../client/src/proto
-protoc -I=. ./blogService.proto \
+protoc -I=. ./chatterish.proto \
   --js_out=import_style=commonjs:../client/src/proto \
   --grpc-web_out=import_style=typescript,mode=grpcwebtext:../client/src/proto
